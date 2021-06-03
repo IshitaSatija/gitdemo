eda2053ef8477ab1d93eb2eb8039136bdcf088f7
Ishita <ishita.satija19@gmail.com>
2021-06-03 14:49:45 +0530

    Added function headers

--- math.py
+++ math.py
@@ -1,8 +1,12 @@
+"Addition function header"
 def add(x,y):
     pass
+"Subtract function header"    
 def subtract(x,y):
     pass
+"Multiplication function header"
 def multiply(x,y):
     pass
+"Division Function header"
 def divide(x,y):
     pass
